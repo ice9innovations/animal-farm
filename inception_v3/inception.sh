cbf527ddbeb2f9b489773cacefd3c4fff9212657
adunderwood <alanunderwood@gmail.com>
2025-09-01 23:49:36 -0700

    Removed Inception v3

--- inception_v3/inception.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-source inception_v3_venv/bin/activate
-source .env
-export LD_LIBRARY_PATH="$(dirname "$0")/inception_v3_venv/lib/python3.9/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH"
-python3 REST.py
